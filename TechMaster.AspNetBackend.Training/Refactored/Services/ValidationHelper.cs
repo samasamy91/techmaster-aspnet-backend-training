eca1d759780ec6ec5ca48cb42856845c2b3dc684
Sama Samy <samasamy68@gmail.com>
2026-07-06 01:52:08 -0700

    ConsoleMenu Done

--- TechMaster.AspNetBackend.Training/Refactored/Services/ValidationHelper.cs
+++ TechMaster.AspNetBackend.Training/Refactored/Services/ValidationHelper.cs
@@ -8,9 +8,22 @@ namespace Refactored.Services
 {
     public class ValidationHelper
     {
-        public string CustomerName { get; set; }
-        public string ProductName { get; set; }
-        public decimal Price { get; set; }  
-        public int Quantity { get; set; }
+        //public string CustomerName { get; set; }
+        //public string ProductName { get; set; }
+        //public decimal Price { get; set; }  
+        //public int Quantity { get; set; }
+        public static bool IsValidName(string name)
+        {
+            return !string.IsNullOrWhiteSpace(name);
+        }
+        public static bool IsValidPrice(decimal price)
+        {
+            return price > 0;
+        }
+        public static bool IsValidQuantity(int quantity)
+        {
+            return quantity>=0;
+        }
+        
     }
 }
--- TechMaster.AspNetBackend.Training/Refactored/UI/ConsoleMenu.cs
+++ TechMaster.AspNetBackend.Training/Refactored/UI/ConsoleMenu.cs
@@ -1,12 +1,76 @@
-﻿using System;
+﻿using Refactored.Models;
+using Refactored.Services;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Refactored.UI
 {
-    internal class ConsoleMenu
+    public class ConsoleMenu
     {
+        private readonly OrderCalculator calc = new OrderCalculator();
+        private readonly ReceiptPrinter printer = new ReceiptPrinter();
+        public void Run()
+        {
+            Console.WriteLine("====== Order Calculator ======");
+            Customer customer = ReadCustomer();
+            Order order = ReadOrder();
+            decimal disc = calc.CalcDiscount(order, customer);
+            decimal afterDisc = order.SubTotal - disc;
+            decimal tax = calc.CalcTax(afterDisc);
+            decimal shipping = calc.CalShipping(afterDisc);
+            decimal finalTotal = calc.CalcFinalTotal(order, customer);
+            printer.Print(customer,order,disc,tax,shipping,finalTotal);
+        }
+        private Customer ReadCustomer()
+        {
+            Customer customer = new Customer();
+            Console.Write("Customer Name: ");
+            customer.Name = Console.ReadLine();
+            while (!ValidationHelper.IsValidName(customer.Name))
+            {
+                Console.Write("Invalid name. Enter again: ");
+                customer.Name = Console.ReadLine();
+            }
+            Console.Write("Customer Type (Regular/Silver/Gold/VIP): ");
+            CutomerType type;
+            while (!Enum.TryParse(Console.ReadLine(), true, out type))
+            {
+                Console.Write("Invalid type. Enter again: ");
+            }
+            customer.Type = type;
+            return customer;
+        }
+        private Order ReadOrder()
+        {
+            Order order = new Order();
+            Console.WriteLine("Product Name: ");
+            while (!ValidationHelper.IsValidName(order.ProductName))
+            {
+                Console.Write("Invalid product name. Enter again: ");
+                order.ProductName = Console.ReadLine();
+            }
+            Console.Write("Price: ");
+            decimal price;
+            while (!decimal.TryParse(Console.ReadLine(), out price) ||
+                   !ValidationHelper.IsValidPrice(price))
+            {
+                Console.Write("Invalid price. Enter again: ");
+            }
+            order.Price = price;
+            Console.Write("Quantity: ");
+            int quantity;
+            while (!int.TryParse(Console.ReadLine(), out quantity) ||
+                   !ValidationHelper.IsValidQuantity(quantity))
+            {
+                Console.Write("Invalid quantity. Enter again: ");
+            }
+            order.Quantity = quantity;
+            return order;
+        }
+
     }
 }
