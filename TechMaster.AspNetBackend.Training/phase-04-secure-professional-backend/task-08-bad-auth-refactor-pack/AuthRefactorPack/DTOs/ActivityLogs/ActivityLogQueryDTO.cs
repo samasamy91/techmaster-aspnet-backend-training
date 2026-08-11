cf38710d6229901120db8a707e9dc9d6df11fdb9
Sama Samy <samasamy68@gmail.com>
2026-08-11 21:38:03 +0300

    Phase 04 Task08 Add Activity Log

---
+++ TechMaster.AspNetBackend.Training/phase-04-secure-professional-backend/task-08-bad-auth-refactor-pack/AuthRefactorPack/DTOs/ActivityLogs/ActivityLogQueryDTO.cs
@@ -0,0 +1,12 @@
+﻿using TrainingCenter.Api.Common;
+
+namespace ActivityTrainingCenter.DTOs.ActivityLogs
+{
+    public class ActivityLogQueryDTO : PagedRequest
+    {
+        public int? UserId { get; set; }
+        public string? EntityName { get; set; }
+        public DateTime? From { get; set; }
+        public DateTime? To { get; set; }
+    }
+}
