4c8119ff8a702c506a0439d06a7668e5466fcfb4
Sama Samy <samasamy68@gmail.com>
2026-08-11 21:39:53 +0300

    Phase 04 Task08 Add Security and DTOs for Refactored code

---
+++ TechMaster.AspNetBackend.Training/phase-04-secure-professional-backend/task-08-bad-auth-refactor-pack/AuthRefactorPack/Security/JwtSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace TrainingCenterAuthTask01.Security
+{
+    public class JwtSettings
+    {
+        public const string SectionName = "Jwt";
+        public string SecretKey { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public int ExpirationMinutes { get; set; }
+    }
+}
